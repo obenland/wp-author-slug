2a864b91ec150ba2413f0890a087a8d6fb9a8df5
Konstantin Obenland <obenland@gmx.de>
2022-11-08 17:04:26 -0800

    Update with latest SVN version

--- uninstall.php
+++ uninstall.php
@@ -1,24 +1,26 @@
 <?php
-//Don't uninstall unless you absolutely want to!
-if ( ! defined( 'WP_UNINSTALL_PLUGIN'  ) ) {
+/**
+ * Uninstall.
+ *
+ * @package WP Author Slug
+ */
+
+// Don't uninstall unless you absolutely want to!
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	wp_die( 'WP_UNINSTALL_PLUGIN undefined.' );
 }
 
-$users	=	get_users( array(
-	'blog_id'	=>	'',
-	'fields'	=>	array( 'ID', 'user_login' )
+$users = get_users( array(
+	'blog_id' => '',
+	'fields'  => array( 'ID', 'user_login' ),
 ) );
 
 foreach ( $users as $user ) {
 	@wp_update_user( array(
-		'ID'			=>	$user->ID,
-		'user_nicename'	=>	sanitize_title( $user->user_login )
+		'ID'            => $user->ID,
+		'user_nicename' => sanitize_title( $user->user_login ),
 	) );
 }
 
 
 /* Goodbye! Thank you for having me! */
-
-
-/* End of file uninstall.php */
-/* Location: ./wp-content/plugins/wp-author-slug/uninstall.php */
--- wp-author-slug.php
+++ wp-author-slug.php
@@ -1,110 +1,99 @@
 <?php
-/** wp-author-slug.php
- *
- * Plugin Name:	WP Author Slug
- * Plugin URI:	http://en.wp.obenland.it/wp-author-slug/?utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-author-slug
- * Description:	Rewrites the author url to NOT display the username but the display name
- * Version:		1.2.2
- * Author:		Konstantin Obenland
- * Author URI:	http://en.wp.obenland.it/?utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-author-slug
+/**
+ * Plugin Name: WP Author Slug
+ * Plugin URI:  http://en.wp.obenland.it/wp-author-slug/?utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-author-slug
+ * Description: Rewrites the author url to NOT display the username but the display name
+ * Version:     3
+ * Author:      Konstantin Obenland
+ * Author URI:  http://en.wp.obenland.it/?utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-author-slug
  * Text Domain: wp-author-slug
  * Domain Path: /lang
- * License:		GPLv2
+ * License:     GPLv2
+ *
+ * @package WP Author Slug
  */
 
-
-if ( ! class_exists( 'Obenland_Wp_Plugins_v300' ) ) {
-	require_once( 'obenland-wp-plugins.php' );
+if ( ! class_exists( 'Obenland_Wp_Plugins_V4' ) ) {
+	require_once 'obenland-wp-plugins.php';
 }
-
 
 register_activation_hook( __FILE__, array(
 	'Obenland_Wp_Author_Slug',
-	'activation'
+	'activation',
 ) );
 
+/**
+ * Class Obenland_Wp_Author_Slug.
+ */
+class Obenland_Wp_Author_Slug extends Obenland_Wp_Plugins_V4 {
 
-class Obenland_Wp_Author_Slug extends Obenland_Wp_Plugins_v300 {
-	
-	
-	///////////////////////////////////////////////////////////////////////////
-	// METHODS, PUBLIC
-	///////////////////////////////////////////////////////////////////////////
-	
 	/**
-	 * Constructor
+	 * Constructor.
 	 *
-	 * @author	Konstantin Obenland
-	 * @since	1.1 - 03.04.2011
-	 * @access	public
-	 *
-	 * @return	Obenland_Wp_Author_Slug
+	 * @author Konstantin Obenland
+	 * @since  1.1 - 03.04.2011
+	 * @access public
 	 */
 	public function __construct() {
-		
 		parent::__construct( array(
-			'textdomain'		=>	'wp-author-slug',
-			'plugin_path'		=>	__FILE__,
-			'donate_link_id'	=>	'XVPLJZ3VH4GCN'
-		));
-		
+			'textdomain'     => 'wp-author-slug',
+			'plugin_path'    => __FILE__,
+			'donate_link_id' => 'XVPLJZ3VH4GCN',
+		) );
+
 		$this->hook( 'pre_user_nicename' );
 	}
-	
-	
+
+
 	/**
-	 * Overwrites the users' nicenames with the users' display name
+	 * Overwrites the users' nicenames with the users' display name.
 	 *
-	 * Only runs on activation of plugin
+	 * Only runs on activation of plugin.
 	 *
-	 * @author	Konstantin Obenland
-	 * @since	1.0 - 19.02.2011
-	 * @access	public
+	 * @author Konstantin Obenland
+	 * @since  1.0 - 19.02.2011
+	 * @access public
 	 * @static
-	 *
-	 * @return	void
 	 */
 	public static function activation() {
-		$users	=	get_users(array(
-			'blog_id'	=>	'',
-			'fields'	=>	array( 'ID', 'display_name' )
-		));
-		
+		$users = get_users( array(
+			'blog_id' => '',
+			'fields'  => array( 'ID', 'display_name' ),
+		) );
+
 		foreach ( $users as $user ) {
-			if ( $user->display_name ) {
+			if ( ! empty( $user->display_name ) ) {
 				@wp_update_user( array(
-					'ID'			=>	$user->ID,
-					'user_nicename'	=>	sanitize_title( $user->display_name )
+					'ID'            => $user->ID,
+					'user_nicename' => sanitize_title( $user->display_name ),
 				) );
 			}
 		}
 	}
-	
-	
+
+
 	/**
-	 * Overwrites the user's nicename with the user's display name
+	 * Overwrites the user's nicename with the user's display name.
 	 *
-	 * Runs every time a user is created or updated
+	 * Runs every time a user is created or updated.
 	 *
-	 * @author	Konstantin Obenland
-	 * @since	1.0 - 19.02.2011
+	 * @author Konstantin Obenland
+	 * @since  1.0 - 19.02.2011
+	 * @access public
 	 *
-	 * @param	string	$name	The default nicename
-	 *
-	 * @return	string	The sanitized nicename
+	 * @param string $name The default nicename.
+	 * @return string The sanitized nicename.
 	 */
 	public function pre_user_nicename( $name ) {
-		
-		if ( $_REQUEST['display_name'] ) {
-			return sanitize_title( $_REQUEST['display_name'] );
+		// phpcs:disable WordPress.VIP.ValidatedSanitizedInput, WordPress.CSRF.NonceVerification.NoNonceVerification
+		if ( ! empty( $_REQUEST['display_name'] ) ) {
+			$name = sanitize_title( $_REQUEST['display_name'] );
 		}
+		// phpcs:enable WordPress.VIP.ValidatedSanitizedInput, WordPress.CSRF.NonceVerification.NoNonceVerification
+
 		return $name;
 	}
-}  // End of class Obenland_Wp_Author_Slug
+}  // End of class Obenland_Wp_Author_Slug.
 
 
-new Obenland_Wp_Author_Slug;
-
-
-/* End of file wp-author-slug.php */
-/* Location: ./wp-content/plugins/wp-author-slug/wp-author-slug.php */
+new Obenland_Wp_Author_Slug();
