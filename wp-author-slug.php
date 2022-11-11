7f2bd8385dbd1f0c4c07afa40abbc12c40deccd7
Konstantin Obenland <obenland@gmx.de>
2022-11-10 21:30:50 -0800

    Move cleanup to deactivation hook

--- wp-author-slug.php
+++ wp-author-slug.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Author Slug
  * Plugin URI:  http://en.wp.obenland.it/wp-author-slug/?utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-author-slug
  * Description: Rewrites the author url to NOT display the username but the display name
- * Version:     3
+ * Version:     4
  * Author:      Konstantin Obenland
  * Author URI:  http://en.wp.obenland.it/?utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-author-slug
  * Text Domain: wp-author-slug
@@ -24,11 +24,6 @@ Obenland_Wp_Author_Slug::get_instance();
  * Overwrites the users' nicenames with the users' display name.
  *
  * Only runs on activation of plugin.
- *
- * @author Konstantin Obenland
- * @since  1.0 - 19.02.2011
- * @access public
- * @static
  */
 function wp_author_slug_activation() {
 	$users = get_users(
@@ -50,3 +45,27 @@ function wp_author_slug_activation() {
 	}
 }
 register_activation_hook( __FILE__, 'wp_author_slug_activation' );
+
+/**
+ * Restores users' nicenames.
+ *
+ * Only runs on activation of plugin.
+ */
+function wp_author_slug_deactivation() {
+	$users = get_users(
+		array(
+			'blog_id' => '',
+			'fields'  => array( 'ID', 'user_login' ),
+		)
+	);
+
+	foreach ( $users as $user ) {
+		wp_update_user(
+			array(
+				'ID'            => $user->ID,
+				'user_nicename' => sanitize_title( $user->user_login ),
+			)
+		);
+	}
+}
+register_deactivation_hook( __FILE__, 'wp_author_slug_deactivation' );
