0d00488f3c69b2030cbfd38849bf80e33677024d
Konstantin Obenland <obenland@gmx.de>
2025-09-08 20:05:58 -0500

    Add conflict detection for author slugs matching page slugs (#12)

--- wp-author-slug.php
+++ wp-author-slug.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Author Slug
  * Plugin URI:  http://en.wp.obenland.it/wp-author-slug/?utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-author-slug
  * Description: Rewrites the author url to NOT display the username but the display name
- * Version:     4
+ * Version:     5
  * Author:      Konstantin Obenland
  * Author URI:  http://en.wp.obenland.it/?utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-author-slug
  * Text Domain: wp-author-slug
@@ -33,15 +33,33 @@ function wp_author_slug_activation() {
 		)
 	);
 
+	$conflicts = array();
+
 	foreach ( $users as $user ) {
 		if ( ! empty( $user->display_name ) ) {
+			$proposed_slug = sanitize_title( $user->display_name );
+
+			// Check for conflicts with existing pages.
+			$existing_page = get_page_by_path( $proposed_slug );
+			if ( $existing_page ) {
+				$conflicts[] = array(
+					'user_id' => $user->ID,
+					'page_id' => $existing_page->ID,
+				);
+			}
+
 			wp_update_user(
 				array(
 					'ID'            => $user->ID,
-					'user_nicename' => sanitize_title( $user->display_name ),
+					'user_nicename' => $proposed_slug,
 				)
 			);
 		}
+	}
+
+	// Store conflicts for admin notices.
+	if ( ! empty( $conflicts ) ) {
+		update_option( 'wp_author_slug_conflicts', $conflicts );
 	}
 }
 register_activation_hook( __FILE__, 'wp_author_slug_activation' );
@@ -49,7 +67,7 @@ register_activation_hook( __FILE__, 'wp_author_slug_activation' );
 /**
  * Restores users' nicenames.
  *
- * Only runs on activation of plugin.
+ * Only runs on deactivation of plugin.
  */
 function wp_author_slug_deactivation() {
 	$users = get_users(
@@ -67,5 +85,8 @@ function wp_author_slug_deactivation() {
 			)
 		);
 	}
+
+	// Clean up stored conflicts.
+	delete_option( 'wp_author_slug_conflicts' );
 }
 register_deactivation_hook( __FILE__, 'wp_author_slug_deactivation' );
